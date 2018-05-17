f811a2a230b83b9f29d7e3d733db6b14b98f5aad
abrarShariar <shariarabrar@gmail.com>
2018-05-18 03:42:52 +0600

    halal push

--- my-redux-app/src/App.js
+++ my-redux-app/src/App.js
@@ -3,22 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+// redux stuffs
+import {Provider} from 'react-redux';
+
+import store from '../store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <PostForm/>
-        <br/><hr/><br/>
-        <Posts/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <PostForm/>
+          <br/><hr/><br/>
+          <Posts/>
+        </div>
+      </Provider>  
     );
   }
 }
